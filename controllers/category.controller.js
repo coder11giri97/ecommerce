0b6a3b80f38de656d8ab2decab1a004d8ee9f2eb
coder11giri97 <girishnakate19@gmail.com>
2022-10-31 14:41:42 +0530

    handler for creating the new category request

---
+++ controllers/category.controller.js
@@ -0,0 +1,33 @@
+/**
+ * this file have logic which is necessary for processing the request
+ * 
+ * handler for creating a new category request
+ */
+
+const db = require("../models");
+const Category = db.category;
+exports.create = (req , res)=>{
+   /**
+    * try to create new category object using request body
+    */
+    
+   const category = {
+       name : req.body.name,
+       description : req.body.description
+   }
+
+   // store this in db
+   Category.create(category).then(category =>{
+       console.log(`category name: [ ${category.name}] got inserted in the db`);
+       res.status(201).send(category);
+
+   }).caatch(err =>{
+       console.log(`issue in inserting the category name: [ ${category.name}].Error message :${err.message}`);
+       res.status(500).send{
+             message : `some internal eroor happened`
+       }
+   })
+
+
+
+}
