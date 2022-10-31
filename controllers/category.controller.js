79613b3f5f06204cd8cab4152f03653003c5dc98
coder11giri97 <girishnakate19@gmail.com>
2022-11-01 03:06:56 +0530

    providing support for updating the category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -21,10 +21,10 @@ exports.create = (req , res)=>{
        console.log(`category name: [ ${category.name}] got inserted in the db`);
        res.status(201).send(category);
 
-   }).caatch(err =>{
+   }).catch(err =>{
        console.log(`issue in inserting the category name: [ ${category.name}].Error message :${err.message}`);
        res.status(500).send({
-             message : `some internal eroor happened`
+             message : 'some internal eroor happened'
        })
    })
 
@@ -44,7 +44,61 @@ exports.findAll = (req, res) =>{
           res.status(200).send(categories);
       }).catch(err=>{
         res.status(500).send({
-          message : `some internal eroor happened`
+          message : 'some internal eroor happened'
       })
     })
+}
+
+/**
+ * handler for getting categories based on yhe id
+ * 
+ */
+ 
+exports.findOne = (req, res) =>{
+     const categoryId = req.params.id;
+
+     Category.findByPk(categoryId).then(categoryId =>{
+          res.status(201).send(categoryId);
+
+     }).catch(err =>{
+        res.status(500).send({
+            message : 'some internal error happened'
+        })
+     })
+}
+
+
+/**
+ * provide support for updating the category
+ */
+
+exports.update = (req, res) =>{
+     /**
+      * need to parse the request body just like post method
+      */
+      const category = {
+        name : req.body.name,
+        description : req.body.description
+    }
+    /**
+     * need to know which category has to be updated
+     */
+    const categoryId = req.params.id ;
+    
+    /**
+     * now update the category
+     */
+      Category.update(category,{
+          where : {id : categoryId},
+          returning : true
+      }).then(updatedCategory =>{
+        res.status(200).send(updatedCategory);
+      }).catch(err =>{
+        res.status(500).send({
+            message : "some internal error happened"
+        })
+      })
+
+
+
 }
