31a3af000e2f7f14e804d59471ae103fe439f1da
coder11giri97 <girishnakate19@gmail.com>
2022-10-31 17:10:10 +0530

    initialised the route

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const serverConfig = require('./configs/server.config');
 
 const app = express();
+
+/**registering body-parser middleware */
+app.use(bodyParser.json());
+
+/**
+ * code for the table initialization
+ */
+const db = require("./models"); // node will find index.js form models folder -- standard naming
+const Category = db.category;
+
+console.log(Category);
+
+/**
+ * create the table
+ */
+db.sequelize.sync({force:true}).then(() => {
+      console.log("table dropped and recreated");
+}).catch(err =>{
+    console.log(err.message);
+})
+
+//initialise the routes
+require('./routes/category.route')(app);
 
 app.listen(serverConfig.PORT,() =>{
     console.log("application started");
